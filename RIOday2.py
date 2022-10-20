7a7833dbcbf076a25ed3b20e084df00a422a3e5d
riofebrian839 <99810147+riofebrian839@users.noreply.github.com>
2022-10-20 08:25:23 -0700

    Add files via upload

---
+++ RIOday2.py
@@ -0,0 +1,8 @@
+nama = 'RIO FEBRIAN'
+usia = 20
+alamat = 'sendana'
+status_menikah = False
+print('NAMA=', nama)
+print('USIA=', usia)
+print('ALAMAT=', alamat)
+print('STATUS MENIKAH=', status_menikah)
