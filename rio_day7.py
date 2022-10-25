9730dd6e15956d8eba002f7f18a41c96d73adabf
riofebrian839 <99810147+riofebrian839@users.noreply.github.com>
2022-10-25 22:18:29 +0700

    Add files via upload

---
+++ rio_day7.py
@@ -0,0 +1,16 @@
+a = 5
+b = 4
+
+penjumlahan = a+b
+pengurangan = a-b
+perkalian = a*b
+pembagian = a/b
+sisa_bagi = a%b
+pangkat = a**b
+
+print("penjumlahan = ", penjumlahan)
+print("pengurangan = ", pengurangan)
+print("perkalian = ", perkalian)
+print("pembagian = ", pembagian)
+print("sisa_bagi = ", sisa_bagi)
+print("pangkat = ", pangkat)
