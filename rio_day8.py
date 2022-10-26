70dbc62595d3a63758c8937f170c5bfcdeb5ce04
riofebrian839 <99810147+riofebrian839@users.noreply.github.com>
2022-10-26 19:53:22 +0700

    Add files via upload

---
+++ rio_day8.py
@@ -0,0 +1,16 @@
+hari_ini = "minggu"
+
+if(hari_ini == "senin"):
+    print("saya akan kuliah")
+if(hari_ini == "selasa"):
+    print("saya akan kuliah")
+if(hari_ini == "rabu"):
+    print("saya akan kuliah")
+if(hari_ini == "kamis"):
+    print("saya akan kuliah")
+if(hari_ini == "jumat"):
+    print("saya akan kuliah")
+if(hari_ini == "sabtu"):
+    print("saya akan kuliah")
+if(hari_ini == "minggu"):
+    print("pergi healing")
