a79f8d3c7ba94ccb50b51e221b532c83050df75f
riofebrian839 <99810147+riofebrian839@users.noreply.github.com>
2022-11-09 20:35:44 +0700

    Add files via upload

---
+++ rio_day22.py
@@ -0,0 +1,9 @@
+jawab = "ya"
+hitung = 0
+
+while(True):
+    hitung = hitung +1
+    jawab= input("masih mauki ulang atau tidak? ")
+    if jawab == "tidak":
+        break
+print("total perulangan : ", hitung)
