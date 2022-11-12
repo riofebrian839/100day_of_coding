db90f6f7ffef2372f92c042dff2736060c8b00e1
riofebrian839 <99810147+riofebrian839@users.noreply.github.com>
2022-11-12 21:41:44 +0700

    Add files via upload

---
+++ rio_day25.py
@@ -0,0 +1,6 @@
+lama_pinjam = int(input("pinjam buku berapa hari? "))
+
+if lama_pinjam >= 3:
+    print('Meminjam Buku Selama {} Gratis'.format(lama_pinjam))
+else:
+    print('Meminjam Buku Selama {} Gratis'.format(lama_pinjam))
