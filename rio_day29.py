933d0b8998cd11d84a3e950b04cc057fb1e979b0
riofebrian839 <99810147+riofebrian839@users.noreply.github.com>
2022-11-16 21:27:28 +0700

    Add files via upload

---
+++ rio_day29.py
@@ -0,0 +1,12 @@
+jarak = int(input("Masukan Jarak = "))
+harga_tiket = int(input("Masukan Harga Tiket = "))
+
+if (jarak>300) or (harga_tiket>500000) :
+    beli = harga_tiket*0.5
+elif (jarak>200) or (harga_tiket>300000) :
+    beli = harga_tiket*0.8
+else :
+    beli = harga_tiket
+    
+print()
+print("Harga Tiket = Rp", beli)
