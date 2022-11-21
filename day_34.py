89ba957888bb95d4a59073ade34fbd1e4b92358d
riofebrian839 <99810147+riofebrian839@users.noreply.github.com>
2022-11-21 23:16:10 +0800

    Add files via upload

---
+++ day_34.py
@@ -0,0 +1,10 @@
+bil1 =int(input("masukkan bilangan 1 = "))
+bil2 =int(input("masukkan bilangan 2 = "))
+if (bil1 > bil2):
+    hasil = "besar"
+
+else :
+    hasil = "kecil"
+
+print()
+print("bil", bil1, "lebih", hasil, "daripada", "bilangan", bil2)
