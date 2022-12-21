94dbac0392da9f7e882f3caa339a234b7772d2d2
riofebrian839 <99810147+riofebrian839@users.noreply.github.com>
2022-12-21 22:18:21 +0800

    Add files via upload

---
+++ day_64.py
@@ -0,0 +1,10 @@
+import calendar
+
+print("========================")
+print("Sekarang Bulan Desember")
+print("========================")
+year = 2022
+month = 12
+hh = calendar.month(year,month)
+print(hh)
+print("========================")
